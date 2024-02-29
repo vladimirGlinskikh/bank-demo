b3ec84027fc70968ba960867a941722a801d3baf
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-02-29 16:06:37 +0300

    Start project

---
+++ src/test/java/kz/zhelezyaka/bankdemo/BankDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.bankdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
